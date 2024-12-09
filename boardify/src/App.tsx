9d6bffa2574c662a068879ed4e683b617458eaae
BluePixelDev <bluepixeldev@gmail.com>
2024-12-09 12:18:32 +0100

    -Added css snippets - Added redux store - File Localisation

--- boardify/src/App.tsx
+++ boardify/src/App.tsx
@@ -1,25 +1,31 @@
 import "./App.css";
-import AppTheme from "@/components/AppTheme";
+import AppTheme from "@/components/theme/AppTheme";
 import GraphNode from "@/components/graph/GraphNode";
 import GraphView from "@/components/graph/GraphView";
+import Snippets from "./components/snippets/AppSnippets";
+import { Provider } from "react-redux";
+import store from "./store"
 
 function App() {
   return (
-    <AppTheme>
-      <GraphView graph={{
-        maxZoom: 5,
-        minZoom: 0.5
-      }}>
-        <GraphNode position={{
-          x: -10,
-          y: 0
-        }} size={{
-          x: 250,
-          y: 250
+    <Provider store={store}>
+      <AppTheme>
+        <Snippets/>
+        <GraphView graph={{
+          maxZoom: 5,
+          minZoom: 0.5
         }}>
-        </GraphNode>
-      </GraphView>
-    </AppTheme>
+          <GraphNode position={{
+            x: -10,
+            y: 0
+          }} size={{
+            x: 250,
+            y: 250
+          }}>
+          </GraphNode>
+        </GraphView>
+      </AppTheme>
+    </Provider>
   );
 }
 
---
+++ boardify/src/redux/snippetSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type CSSSnippet = {
+  id: string,
+  name: string,
+  snippet: string
+}
+
+interface CSSSnippetState {
+  snippets: CSSSnippet[]
+}
+
+const initialState: CSSSnippetState = {
+  snippets: [
+    {
+      id: "test",
+      name: "Test Snippet",
+      snippet: ""
+    }
+  ],
+};
+
+const cssSnippetsSlice = createSlice({
+  name: "cssSnippets",
+  initialState,
+  reducers: {
+    addSnippet: (state, action) => {
+      state.snippets.push(action.payload); // Add new snippet
+    },
+    removeSnippet: (state, action) => {
+      state.snippets = state.snippets.filter(snippet => snippet.id !== action.payload);
+    },
+    updateSnippet: (state, action) => {
+      const index = state.snippets.findIndex(snippet => snippet.id === action.payload.id);
+      if (index !== -1) {
+        state.snippets[index] = action.payload;
+      }
+    }
+  }
+});
+
+export type { CSSSnippet, CSSSnippetState };
+export const { addSnippet, removeSnippet, updateSnippet } = cssSnippetsSlice.actions;
+export default cssSnippetsSlice.reducer;
